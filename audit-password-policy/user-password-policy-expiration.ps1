b288aa5a59023b0433e342cdff96a879f8a60e3a
ka2er <personseb+git@gmail.com>
2016-06-07 14:33:35 +0200

    draft template for password policy

---
+++ audit-password-policy/user-password-policy-expiration.ps1
@@ -0,0 +1,23 @@
+if($msolcred -eq $null) {
+	$msolcred = get-credential
+}
+
+if($msol_session -eq $null) {
+    write-host "Connecting to office 365..."
+    Import-Module MSOnline
+    $msol_session = Connect-MsolService -Credential $msolcred
+}
+
+
+Get-MsolPasswordPolicy -DomainName acme.com
+
+#Get All Licensed Users
+$users = Get-MsolUser -All | Where-Object {$_.isLicensed -eq $true}
+foreach ($user in $users) {
+	PasswordNeverExpires
+	StrongPasswordRequired
+	StrongPasswordRequired
+}
+
+#write-output "Line #$i OK1:$($UPN): MAJ mdp policy + env : Resultat ($?)"
+#write-output ("Line #$i OK2:password maj $($UPN) = $x : Resultat ($?)")
