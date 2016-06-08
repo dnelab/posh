a78664783a9b821e74a0523c6eeed9f8431f3e67
ka2er <personseb+git@gmail.com>
2016-06-08 16:40:37 +0200

    top5 oldest password user

---
+++ audit-password-policy/get-oldest-passwords.ps1
@@ -0,0 +1,29 @@
+try {
+    Get-MsolDomain -ErrorAction Stop > $null
+} catch {
+    if($msolcred -eq $null) {
+	    $msolcred = get-credential
+    }
+
+    if($msol_session -eq $null) {
+        write-host "Connecting to office 365..."
+        Import-Module MSOnline
+        $msol_session = Connect-MsolService -Credential $msolcred
+    }
+}
+
+$top5 = (Get-MsolUser -All | Where-Object {$_.isLicensed -eq $true} | Sort-Object LastPasswordChangeTimeStamp | Select-Object -First 5 DisplayName,LastPasswordChangeTimeStamp)
+
+
+$now = (get-date)
+foreach($item in $top5) {
+    $age = $now - ($item.LastPassWordChangeTimeStamp)
+    #$item['age'] = $age
+    $item | Add-Member PasswordAge $age.TotalDays
+}
+
+Write-Host "TOP 5 oldest password" -ForegroundColor DarkYellow
+$top5
+
+
+
