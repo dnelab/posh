ecc162b3ef851a7fd91362f1163e0465c827ca41
ka2er <personseb+git@gmail.com>
2017-03-29 16:35:30 +0200

    enable skype mfa authentification

---
+++ skype-mfa-enable/skype-mfa-enable.ps1
@@ -0,0 +1,11 @@
+﻿if($UserCredential -eq $null) {
+    $UserCredential = Get-Credential
+}
+
+if($Session -eq $null) {
+    $Session = New-CsOnlineSession -Credential $UserCredential -Verbose
+    Import-PSSession $Session
+}
+#Get-CsOAuthConfiguration
+Set-CsOAuthConfiguration -ClientAdalAuthOverride Allowed
+Get-CsOAuthConfiguration
